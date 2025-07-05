d5009638fcb93abade7348375c8d6e4c5147a4ea
OmarChouchane <omar.ch52831@gmail.com>
2025-07-05 16:16:17 +0100

    page.tsx navbar style test

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,63 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+
 export default function Home() {
   return (
     <nav className="bg-green lg:mx-32 sm:mx-8 md:mx-12 lg:mt-8 md:mt-10 px-4 sm:px-4 lg:px-4 rounded-lg lg:rounded-xl border border-border-white">
       <div className="flex items-center justify-between lg:h-20 md:h-20 sm:h-18 px-4 sm:px-1 lg:px-1 mx-auto">
         <div className="flex-shrink-0">
-          <a href="/">
-            <img
-              className="h-12 w-12"
-              src="logoCarbonJar.svg"
+          <Link href="/">
+            <Image
+              src="/logoCarbonJar.svg"
               alt="logo"
               width={48}
               height={48}
+              className="h-12 w-12"
             />
-          </a>
+          </Link>
         </div>
 
         {/* Desktop menu links */}
         <div className="hidden md:flex flex-1 justify-center">
           <div className="items-baseline lg:space-x-4 flex justify-center">
-            <a href="/" className="text-white hover:text-light-green px-3 py-2 rounded-md font-inter">
+            <Link
+              href="/"
+              className="text-white hover:text-light-green px-3 py-2 rounded-md font-inter"
+            >
               Home
-            </a>
-            <a href="/about" className="text-white hover:text-light-green px-3 py-2 rounded-md font-inter">
+            </Link>
+            <Link
+              href="/about"
+              className="text-white hover:text-light-green px-3 py-2 rounded-md font-inter"
+            >
               About us
-            </a>
-            <a href="/expertise" className="text-white hover:text-light-green px-3 py-2 rounded-md font-inter">
+            </Link>
+            <Link
+              href="/expertise"
+              className="text-white hover:text-light-green px-3 py-2 rounded-md font-inter"
+            >
               Expertise
-            </a>
-            <a href="/trainings" className="text-white hover:text-light-green px-3 py-2 rounded-md font-inter">
+            </Link>
+            <Link
+              href="/trainings"
+              className="text-white hover:text-light-green px-3 py-2 rounded-md font-inter"
+            >
               Trainings
-            </a>
-            <a href="/" className="text-white hover:text-light-green px-3 py-2 rounded-md font-inter">
+            </Link>
+            <Link
+              href="/"
+              className="text-white hover:text-light-green px-3 py-2 rounded-md font-inter"
+            >
               Resources
-            </a>
-            <a href="/" className="text-white hover:text-light-green px-3 py-2 rounded-md font-inter">
+            </Link>
+            <Link
+              href="/"
+              className="text-white hover:text-light-green px-3 py-2 rounded-md font-inter"
+            >
               Careers
-            </a>
+            </Link>
           </div>
         </div>
 
@@ -49,7 +72,11 @@ export default function Home() {
 
         {/* Mobile menu icon (static, no toggle) */}
         <div className="-mr-2 flex md:hidden">
-          <button type="button" className="p-2 text-white" aria-label="Open menu">
+          <button
+            type="button"
+            className="p-2 text-white"
+            aria-label="Open menu"
+          >
             <svg
               className="h-6 w-6 text-white"
               xmlns="http://www.w3.org/2000/svg"
@@ -60,7 +87,12 @@ export default function Home() {
               width={24}
               height={24}
             >
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
             </svg>
           </button>
         </div>
@@ -68,24 +100,42 @@ export default function Home() {
 
       {/* Mobile menu (always visible here) */}
       <div className="md:hidden bg-green px-2 pt-2 pb-3 space-y-1 sm:px-3">
-        <a href="/" className="text-white block px-3 py-2 text-base font-medium">
+        <Link
+          href="/"
+          className="text-white block px-3 py-2 text-base font-medium"
+        >
           Home
-        </a>
-        <a href="/about" className="text-white block px-3 py-2 text-base font-medium">
+        </Link>
+        <Link
+          href="/about"
+          className="text-white block px-3 py-2 text-base font-medium"
+        >
           About us
-        </a>
-        <a href="/expertise" className="text-white block px-3 py-2 text-base font-medium">
+        </Link>
+        <Link
+          href="/expertise"
+          className="text-white block px-3 py-2 text-base font-medium"
+        >
           Expertise
-        </a>
-        <a href="/trainings" className="text-white block px-3 py-2 text-base font-medium">
+        </Link>
+        <Link
+          href="/trainings"
+          className="text-white block px-3 py-2 text-base font-medium"
+        >
           Trainings
-        </a>
-        <a href="/" className="text-white block px-3 py-2 text-base font-medium">
+        </Link>
+        <Link
+          href="/"
+          className="text-white block px-3 py-2 text-base font-medium"
+        >
           Resources
-        </a>
-        <a href="/" className="text-white block px-3 py-2 text-base font-medium">
+        </Link>
+        <Link
+          href="/"
+          className="text-white block px-3 py-2 text-base font-medium"
+        >
           Careers
-        </a>
+        </Link>
       </div>
     </nav>
   );
