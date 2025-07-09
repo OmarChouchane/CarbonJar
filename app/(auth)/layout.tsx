0bd2cf3484817cbf51007adf1234e425c2ff33a2
OmarChouchane <omar.ch52831@gmail.com>
2025-07-09 19:26:52 +0100

    ui fixes
    padding top for mobile applied on auth pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,24 +1,28 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="min-h-screen w-full bg-dotted-pattern bg-cover bg-center">
+    <div
+      className="min-h-screen w-full bg-dotted-pattern bg-cover bg-center"
+      data-auth-page
+    >
       {/* Navbar */}
       <nav className="bg-green lg:mx-32 sm:mx-8 md:mx-12 px-4 sm:px-4 lg:px-4 rounded-b-xl lg:rounded-b-3xl border border-border-white relative z-50">
         <div className="flex items-center justify-center lg:h-20 md:h-20 sm:h-30 h-17 px-4 sm:px-1 lg:px-1 mx-auto">
-            <Link href="/">
-              <Image
+          <Link href="/">
+            <Image
               className="h-40 w-50"
               src="/CarbonJar3.svg"
               alt="CarbonJar"
               width={48}
               height={30}
               priority
-              />
-            </Link>
-            </div>
+            />
+          </Link>
+        </div>
       </nav>
 
       {/* Main content */}
